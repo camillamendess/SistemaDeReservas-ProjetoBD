a8260797afcedd257798fb2368251483ca2d056d
Camilla <milla_mendes83@hotmail.com>
2024-01-20 13:45:33 -0300

    add media queries

--- js/make-script.js
+++ js/make-script.js
@@ -1,14 +1,45 @@
-console.log("Ops");
-
+// Aguarda o evento de carregamento do DOM antes de executar o código
 window.addEventListener('DOMContentLoaded', function(){
+
+    // Declaração de uma função chamada initializeSlick
     function initializeSlick(){
+
+        // Inicializa o plugin Slick no elemento com a classe 'blocks-container'
         $('.blocks-container').slick({
+            
             // Configurações do Slick
-            infinite: false,
-            slidesToShow: 3,
-            slidesToScroll: 1
+            infinite: false,       // Permite ou impede a rotação infinita de slides
+            slidesToShow: 3,        // Número de slides a serem exibidos ao mesmo tempo
+            slidesToScroll: 1,       // Número de slides a serem percorridos ao avançar/retroceder
+            responsive: [
+                {
+                  breakpoint: 1020,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                  }
+                },
+                {
+                  breakpoint: 600,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                  }
+                },
+                {
+                  breakpoint: 375,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                  }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+              ]
         });
     }
 
+    // Chama a função initializeSlick para aplicar as configurações do Slick após o carregamento do DOM
     initializeSlick();
 });
